f4f433592b529638eea83269d9e76eb02cf139ea
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-06-20 22:45:27 -0400

    Refactor: created padZero function to reduce redundantcy
    - refactor formatTime function
    - Add padZero function for better reusability

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-
+// button variables
 const lapBtnEle = document.querySelector(".lap-btn");
 const resetBtnEle = document.querySelector(".reset-btn");
 const stopBtnEle = document.querySelector(".stop-btn");
 const startBtnEle = document.querySelector(".start-btn");
 
 let displayTimerEle = document.querySelector(".display-timer");
+
+
+
 
 // \*---------------------*/
 // EVENT LISTENERS
@@ -23,6 +26,7 @@ resetBtnEle.addEventListener("click", resetTime)
 let stopTimer = false;
 let resetTimer = null;
 
+
 // timer object created for time manipulation
 const timer = {
   second: 0,
@@ -32,42 +36,46 @@ const timer = {
 }
 
 
+function padZero(typeOfTime) {
+  const pad0Time = typeOfTime < 10 ? `0${typeOfTime}` : typeOfTime;
+  return pad0Time;
+}
+
 
 function formatTime() {
-  timer.second ++;
-// reset seconds if timer gets to 59 seconds and increment minutes every each time
-timer.second > 59 ? (timer.second = 0, timer.minute++) : null;
-// reset minute if timer gets to 60 minutes and increment hour each time
-timer.minute > 59 ? (timer.minute = 0, timer.hour++) : null;
+  timer.second++;
+  // reset seconds if timer gets to 59 seconds and increment minutes every each time
+  timer.second > 59 ? (timer.second = 0, timer.minute++) : null;
+  // reset minute if timer gets to 60 minutes and increment hour each time
+  timer.minute > 59 ? (timer.minute = 0, timer.hour++) : null;
 
   // time check condition to see if time is over certain amount to include 0 padding
-  const seconds0Pad = `${timer.second < 10 ? `0${timer.second}` : timer.second}`;
-  const minutes0Pad = `${timer.minute < 10 ? `0${timer.minute}` : timer.minute}`;
-  const hours0Pad = `${timer.hour < 10 ? `0${timer.hour}` : timer.hour}`;
 
-displayTimerEle.textContent = `${hours0Pad}:${minutes0Pad}:${seconds0Pad}`
+  displayTimerEle.textContent = `${padZero(timer.hour)}:${padZero(timer.minute)}:${padZero(timer.second)}`
 }
 
 
 function startTime() {
   startBtnEle.removeEventListener("click", startTime)
-  stopTimer = setInterval(formatTime , timer.SECONDS_TIME);  
+  stopTimer = setInterval(formatTime, timer.SECONDS_TIME);
 }
 
 function stopTime() {
-  if(stopTimer) {
+  if (stopTimer) {
     clearInterval(stopTimer);
   }
   startBtnEle.addEventListener("click", startTime);
 }
 
 function resetTime() {
-clearInterval(stopTimer);
-clearInterval(formatTime,0);
-timer.second = 0;
-timer.minute = 0;
-timer.hour = 0;
-displayTimerEle.textContent = '00:00:00'
-startBtnEle.addEventListener("click", startTime);
+  clearInterval(stopTimer);
+  clearInterval(formatTime, 0);
+  timer.second = 0;
+  timer.minute = 0;
+  timer.hour = 0;
+  displayTimerEle.textContent = '00:00:00'
+  startBtnEle.addEventListener("click", startTime);
 }
+
+
 
